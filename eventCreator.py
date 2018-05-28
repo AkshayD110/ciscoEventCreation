95018d456c3420dcfadcfb27aa6fe077a6987687
ADeshpande <Vamosrafa@11>
2018-05-28 15:01:31 +0530

    initial implementation

--- eventCreator.py
+++ eventCreator.py
@@ -22,16 +22,25 @@ class eventCreator:
     room_id = sys.argv[2]
 
     def msg_distribution(self):
+        normaltext_count = round((self.msg_count*80)/100)
+        markdowntext_count = round((self.msg_count*17.5)/100)
+        filetransfer_count = round((self.msg_count*1.5)/100)
+        self.all_restricted_msgs()
+
+
+    def normaltext_msg(self, text):
         pass
 
-    def normaltext_msg(self):
+    def markdowntext_msg(self, text):
         pass
 
-    def markdowntext_msg(self):
+    def filetrasfer_msg(self, text):
         pass
 
-    def filetrasfer_msg(self):
-        pass
+    def all_restricted_msgs(self):
+        restrictedwords_count = round((self.msg_count * 1) / 100)
+        restricted_normal = round((restrictedwords_count * 0.5) / 100)
+
 
 
 
