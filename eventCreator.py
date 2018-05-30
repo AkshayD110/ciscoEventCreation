130789b4155c992335c59606b815483c4195dc5d
ADeshpande <Vamosrafa@11>
2018-05-30 16:44:57 +0530

    multi-threading implementation done

--- eventCreator.py
+++ eventCreator.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 import sys
 from ciscosparkapi import CiscoSparkAPI
-from multiprocessing import Process
+import threading
 
 class eventCreator:
 
@@ -33,20 +33,23 @@ class eventCreator:
         filetransfer_count = round((self.msgcount*1.5)/100)
         print(f"normal text will be : {normaltext_count}, markdown text will be : {markdowntext_count}, file transfer count will be :{filetransfer_count}")
 
-        self.normaltext_msg(f"msg here : ", normaltext_count)
-        self.markdowntext_msg(f"msg here : ", markdowntext_count)
-        self.filetrasfer_msg(f"msg here : ", filetransfer_count)
+        t1 = threading.Thread(target=self.normaltext_msg(f"msg here : ", normaltext_count)).start()
+        t2 = threading.Thread(target=self.markdowntext_msg(f"msg here : ", markdowntext_count)).start()
+        t3 = threading.Thread(target=self.filetrasfer_msg(f"msg here : ", filetransfer_count)).start()
+        # self.normaltext_msg(f"msg here : ", normaltext_count)
+        # self.markdowntext_msg(f"msg here : ", markdowntext_count)
+        # self.filetrasfer_msg(f"msg here : ", filetransfer_count)
 
         #self.all_restricted_msgs()
 
 
     def normaltext_msg(self, text, count):
         for i in range(count):
-            message = self.api.messages.create(self.roomid, text=f"This is new - {text} {count}")
+            message = self.api.messages.create(self.roomid, text=f"This is new - {text} {i}")
 
     def markdowntext_msg(self, text, count):
         for i in range(count):
-            message = self.api.messages.create(self.roomid, markdown=f"**This is new - {text} {count}**" )
+            message = self.api.messages.create(self.roomid, markdown=f"**This is new - {text} {i}**" )
 
     def filetrasfer_msg(self, text, count):
         for i in range(count):
