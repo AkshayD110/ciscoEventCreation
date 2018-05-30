30f48f8604a35f03531ee4e89b92b22941e84c73
ADeshpande <Vamosrafa@11>
2018-05-30 16:14:52 +0530

    loops implementation modified

--- eventCreator.py
+++ eventCreator.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import sys
 from ciscosparkapi import CiscoSparkAPI
+from multiprocessing import Process
 
 class eventCreator:
 
@@ -25,30 +26,31 @@ class eventCreator:
     api = CiscoSparkAPI(access_token=sys.argv[1])
     #demo_room = api.rooms.get(room_id)
 
+
     def msg_distribution(self):
         normaltext_count = round((self.msgcount*80)/100)
         markdowntext_count = round((self.msgcount*17.5)/100)
         filetransfer_count = round((self.msgcount*1.5)/100)
         print(f"normal text will be : {normaltext_count}, markdown text will be : {markdowntext_count}, file transfer count will be :{filetransfer_count}")
 
-        for i in range(normaltext_count):
-            self.normaltext_msg(f"new msg count : {i}")
-        for i in range(markdowntext_count):
-            self.markdowntext_msg(f"new msg count : {i}")
-        for i in range(filetransfer_count):
-            self.filetrasfer_msg(f"new msg count : {i}")
+        self.normaltext_msg(f"msg here : ", normaltext_count)
+        self.markdowntext_msg(f"msg here : ", markdowntext_count)
+        self.filetrasfer_msg(f"msg here : ", filetransfer_count)
 
         #self.all_restricted_msgs()
 
 
-    def normaltext_msg(self, text):
-        message = self.api.messages.create(self.roomid, text=f"This is new - {text}")
+    def normaltext_msg(self, text, count):
+        for i in range(count):
+            message = self.api.messages.create(self.roomid, text=f"This is new - {text} {count}")
 
-    def markdowntext_msg(self, text):
-        message = self.api.messages.create(self.roomid, markdown=f"**This is new - {text}**" )
+    def markdowntext_msg(self, text, count):
+        for i in range(count):
+            message = self.api.messages.create(self.roomid, markdown=f"**This is new - {text} {count}**" )
 
-    def filetrasfer_msg(self, text):
-        message = self.api.messages.create(self.roomid, text="welcome to Python", files=["http://hanassets.nd.gov/images/product/test.png"])
+    def filetrasfer_msg(self, text, count):
+        for i in range(count):
+            message = self.api.messages.create(self.roomid, text="welcome to Python", files=["http://hanassets.nd.gov/images/product/test.png"])
 
 
     def all_restricted_msgs(self):
