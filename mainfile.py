fa193f4a5391e8485d0339dfe2d707a03adae244
ADeshpande <Vamosrafa@11>
2018-05-30 13:49:10 +0530

    readme update

--- mainfile.py
+++ mainfile.py
@@ -6,9 +6,6 @@ def main():
 
     eventCreator_obj = eventCreator.eventCreator(number_of_msgsToCreate)
     eventCreator_obj.msg_distribution()
-    #eventCreator_obj.normaltext_msg("akshay")
-    #eventCreator_obj.filetrasfer_msg("hi")
-    #eventCreator_obj.markdowntext_msg("check check")
     print("done")
 
 if __name__ == '__main__':
