f397f78102efe836c991bb5688c1b684549a202a
akshayd110 <akshayd110@gmail.com>
2018-06-06 15:22:33 +0530

    readme change

--- mainfile.py
+++ mainfile.py
@@ -2,7 +2,7 @@
 # File name: mainfile.py           #
 # Author: Akshay Deshpande         #
 # Date created : 22nd May 2018     #
-# Last modified : 30th May 2018    #
+# Last modified : 6th June 2018    #
 ####################################
 
 
