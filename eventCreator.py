ca3c8c564531489a940e50b35f357b5e4166210e
akshayd110 <akshayd110@gmail.com>
2018-09-01 22:46:21 +0530

    support to mutli rooms creation of data

--- eventCreator.py
+++ eventCreator.py
@@ -14,6 +14,7 @@ from random import *
 import time
 import config
 
+
 class eventCreator:
 
     def __init__(self, msgcount):
@@ -30,69 +31,81 @@ class eventCreator:
     def msgcount(self, msgcount):
         self._msgcount = msgcount
 
-
     # include the details in property file
-    accesstoken = "Bearer " + str(sys.argv[1])
-    roomid = sys.argv[2]
-    api = CiscoSparkAPI(access_token=sys.argv[1])
-    #print(f'accesstoken: {accesstoken}, room_id : {roomid}')
-    #demo_room = api.rooms.get(room_id)
+    # accesstoken = "Bearer OGU5Y2Q4ODMtOWE4Yi00OWI1LWEzMGYtMDJjMmYyYjcwOGZiMTc2N2U2YTYtMDEy"
+    # roomid = sys.argv[2]
+    roomid = config.roomids
+    api = CiscoSparkAPI(access_token=config.userid)
 
+    # print(f'accesstoken: {accesstoken}, room_id : {roomid}')
+    # demo_room = api.rooms.get(room_id)
 
     def msg_distribution(self):
-        #normaltext_count = round((self.msgcount*80)/100)
+        # normaltext_count = round((self.msgcount*80)/100)
         normaltext_count = config.INPUT_CONFIG['normal_messages']
         markdowntext_count = config.INPUT_CONFIG['markdowntext_messages']
         filetransfer_count = config.INPUT_CONFIG['filetransfer_messages']
-        print(f"normal text will be : {normaltext_count}, markdown text will be : {markdowntext_count}, file transfer count will be :{filetransfer_count}")
+        print(
+            f"normal text will be : {normaltext_count}, markdown text will be : {markdowntext_count}, file transfer count will be :{filetransfer_count}")
 
-        #below implementaition introduces multiThreading
+        # below implementaition introduces multiThreading
         t1 = threading.Thread(target=self.normaltext_msg(f"msg here : ", normaltext_count)).start()
         t2 = threading.Thread(target=self.markdowntext_msg(f"msg here : ", markdowntext_count)).start()
         t3 = threading.Thread(target=self.filetrasfer_msg(f"msg here : ", filetransfer_count)).start()
         t4 = threading.Thread(target=self.all_restricted_msgs()).start()
 
-        #self.all_restricted_msgs()
-
+        # self.all_restricted_msgs()
 
     def normaltext_msg(self, text, count):
         for i in range(count):
-            message = self.api.messages.create(self.roomid, text=f"some generic text here - {text} {i}")
+            current_room = choice(self.roomid)
+            message = self.api.messages.create(current_room, text=f"some generic text here - {text} {i}")
             time.sleep(1)
 
     def markdowntext_msg(self, text, count):
         for i in range(count):
-            message = self.api.messages.create(self.roomid, markdown=f"**some generic text here - {text} {i}**" )
+            current_room = choice(self.roomid)
+            message = self.api.messages.create(current_room, markdown=f"**some generic text here - {text} {i}**")
             time.sleep(1)
 
     def filetrasfer_msg(self, text, count):
         for i in range(count):
-            message = self.api.messages.create(self.roomid, text="some generic text here ", files=["http://hanassets.nd.gov/images/product/test.png"])
+            current_room = choice(self.roomid)
+            message = self.api.messages.create(current_room, text="some generic text here ",
+                                               files=["http://hanassets.nd.gov/images/product/test.png"])
             time.sleep(2)
 
     def all_restricted_msgs(self):
-        #Generates 40k restricted phrases and picks one
+        # Generates 40k restricted phrases and picks one
         listofphrase = [f'ThisIsABigResTrictedPhrase{x}' for x in range(1, 40000)]
 
         restrictedwords_count = config.INPUT_CONFIG['restricted_messages']
         restricted_normal = round((restrictedwords_count * 50) / 100)
         restricted_markdown = round((restrictedwords_count * 25) / 100)
-        restricted_files =  round((restrictedwords_count * 25) / 100)
-        print(f"Here are the rest_wordsCount :{restrictedwords_count}, restricted_normal :{restricted_normal}, restricted_markdown :{restricted_markdown}, restricted_files :{restricted_files}")
+        restricted_files = round((restrictedwords_count * 25) / 100)
+        print(
+            f"Here are the rest_wordsCount :{restrictedwords_count}, restricted_normal :{restricted_normal}, restricted_markdown :{restricted_markdown}, restricted_files :{restricted_files}")
 
         for i in range(restricted_normal):
             restricted_word = choice(listofphrase)
-            message = self.api.messages.create(self.roomid, text=f"some generic text here and restword : {restricted_word}")
+            current_room = choice(self.roomid)
+            message = self.api.messages.create(current_room,
+                                               text=f"some generic text here and restword : {restricted_word}")
             time.sleep(1)
 
         for i in range(restricted_markdown):
             restricted_word = choice(listofphrase)
-            message = self.api.messages.create(self.roomid, markdown=f"**some generic text here and restword : {restricted_word}**")
+            current_room = choice(self.roomid)
+            message = self.api.messages.create(current_room,
+                                               markdown=f"**some generic text here and restword : {restricted_word}**")
             time.sleep(1)
 
         for i in range(restricted_files):
             restricted_word = choice(listofphrase)
-            message = self.api.messages.create(self.roomid, text=f"some generic text here and restword : {restricted_word}", files=["http://hanassets.nd.gov/images/product/test.png"])
+            current_room = choice(self.roomid)
+            message = self.api.messages.create(current_room,
+                                               text=f"some generic text here and restword : {restricted_word}",
+                                               files=["http://hanassets.nd.gov/images/product/test.png"])
             time.sleep(2)
 
         print("done with creating restricted phrases")
