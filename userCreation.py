1b0eb8bb0e54bc90f6acb8054b48077a35faf1a9
akshayd110 <akshayd110@gmail.com>
2018-09-04 02:32:49 +0530

    user creation

--- userCreation.py
+++ userCreation.py
@@ -1,4 +1,7 @@
 from ciscosparkapi import CiscoSparkAPI
+import config
+from random import choice
+import time
 
 class userCreation:
     def __init__(self, usercount):
@@ -15,4 +18,16 @@ class userCreation:
     def usercount(self, usercount):
         self._usercount=usercount
 
-    
+    roomid = config.roomids
+    api = CiscoSparkAPI(access_token=config.userid)
+
+    def createUsers(self):
+        usercount=config.INPUT_CONFIG['usercount']
+        for i in range(usercount):
+            currentroom = choice(self.roomid)
+            user = self.api.memberships.create(roomId=currentroom, personEmail=f"dumyneww{i}@actiance.com", isModerator=False)
+            time.sleep(2)
+
+
+
+
